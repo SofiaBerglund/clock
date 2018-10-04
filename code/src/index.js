02d83f22d9b4ed2d3b8e84f6d11154dba3620629
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-04 12:50:24 +0200

    clock commit

--- code/src/index.js
+++ code/src/index.js
@@ -4,15 +4,50 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount() {
+    this.resumeTime()
+  }
+
+  resumeTime = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    this.stopTime()
+  }
+
+  stopTime = () => {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <div className="clock-container">
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+        <div className="buttons-container">
+          <button onClick={this.stopTime}>Stop time</button>
+          <button onClick={this.resumeTime}>Resume time</button>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock/>, document.getElementById("root"))
