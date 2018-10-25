064697be691a5591211ea0c1145c5c991fa00c4d
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-25 17:41:51 +0200

    styling

--- code/src/index.js
+++ code/src/index.js
@@ -35,8 +35,8 @@ class Clock extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
+      <div className="clock-app">
+        <h1>Welcome to time</h1>
         <div className="clock-container">
           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
